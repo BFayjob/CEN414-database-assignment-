d0b187b391290eb12992ef6b02d289ae20404f8b
BFayjob <93921990+BFayjob@users.noreply.github.com>
2021-12-02 08:31:39 +0100

    Delete SQLQuery1.sql

--- SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [Country_or_Area]
-      ,[Commodity_Transaction]
-      ,[Year]
-      ,[Unit]
-      ,[Quantity]
-      ,[Quantity_Footnotes]
-  FROM [CEN414Assignment].[dbo].[total refinary output]
