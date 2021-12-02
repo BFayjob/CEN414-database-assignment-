a26c9814eb0136dc28ad419079fe2d6d79dfe759
BFayjob <93921990+BFayjob@users.noreply.github.com>
2021-12-02 08:23:26 +0100

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [Country_or_Area]
+      ,[Commodity_Transaction]
+      ,[Year]
+      ,[Unit]
+      ,[Quantity]
+      ,[Quantity_Footnotes]
+  FROM [CEN414Assignment].[dbo].[total refinary output]
